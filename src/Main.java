3c859e394f4a92ecf30f8d17a9eaea57d4e20675
Lebot <alexsey.e@gmail.com>
2023-02-09 13:07:59 +0200

    9

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,23 @@
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        MyHashMap<String, Integer> userphone = new MyHashMap<>();
-        userphone.put("Alex" , 0524);
-
+        MyHashMap<String, Integer> userPhones = new MyHashMap<>();
+        userPhones.put("Alex" , 340);
+        userPhones.put("Edi" , 456);
+        userPhones.put("Alina" , 123);
+        userPhones.put("Daria" , 777);
+        userPhones.put("Victoria" , 985);
+        System.out.println("userPhones = " + userPhones);
+        userPhones.remove("Daria");
+        System.out.println("userPhones = " + userPhones);
+        System.out.println("userPhones.get(\"Victoria\") = " + userPhones.get("Victoria"));
+        //userPhones.clear();
+       // System.out.println("userPhones = " + userPhones);
+        System.out.println("userPhones.size() = " + userPhones.size());
 
 
     }
+
 }
+
--- src/MyHashMap.java
+++ src/MyHashMap.java
@@ -1,8 +1,10 @@
+import java.util.HashMap;
+
 import static java.util.Objects.hash;
 
 class MyHashMap <K, V> {
     private HashMap<K, V>[] table;
-    private int capacity = 100;
+    private int capacity = 4;
 
     static class HashMap<K, V> {
         K key;
@@ -17,51 +19,115 @@ class MyHashMap <K, V> {
 
     }
 
+    public void clear() {
+        int modCount = 0;
+        modCount++;
+        HashMap[] tab = table;
+        for (int i = 0; i < tab.length; i++)
+            tab[i] = null;
+        capacity = 0;
+    }
+
     @SuppressWarnings("unchecked")
     public MyHashMap() {
-        table = new HashMap[ capacity];
+        table = new HashMap[capacity];
 
     }
 
-    public void put(K newKey, V data){
-        if(newKey==null)
+    public void put(K newKey, V data) {
+        if (newKey == null)
             return;    //does not allow to store null.
 
         //calculate hash of key.
-        int hash=hash(newKey);
+        int hash = hash(newKey) % capacity;
         //create new entry.
-        HashMap<K,V> newEntry = new HashMap<>(newKey, data, null);
+        HashMap<K, V> newEntry = new HashMap<>(newKey, data, null);
 
         //if table location does not contain any entry, store entry there.
-        if(table[hash] == null){
+        if (table[hash] == null) {
             table[hash] = newEntry;
-        }else{
-            HashMap<K,V> previous = null;
-            HashMap<K,V> current = table[hash];
+        } else {
+            HashMap<K, V> previous = null;
+            HashMap<K, V> current = table[hash];
 
-            while(current != null){ //we have reached last entry of bucket.
-                if(current.key.equals(newKey)){
-                    if(previous==null){  //node has to be insert on first of bucket.
-                        newEntry.next=current.next;
-                        table[hash]=newEntry;
+            while (current != null) { //we have reached last entry of bucket.
+                if (current.key.equals(newKey)) {
+                    if (previous == null) {  //node has to be insert on first of bucket.
+                        newEntry.next = current.next;
+                        table[hash] = newEntry;
                         return;
-                    }
-                    else{
-                        newEntry.next=current.next;
-                        previous.next=newEntry;
+                    } else {
+                        newEntry.next = current.next;
+                        previous.next = newEntry;
                         return;
                     }
                 }
-                previous=current;
+                previous = current;
                 current = current.next;
             }
             previous.next = newEntry;
         }
     }
 
+    public boolean remove(K deleteKey) {
+
+        int hash = hash(deleteKey) % capacity;
+
+        if (table[hash] == null) {
+            return false;
+        } else {
+            HashMap<K, V> previous = null;
+            HashMap<K, V> current = table[hash];
+
+            while (current != null) { //we have reached last entry node of bucket.
+                if (current.key.equals(deleteKey)) {
+                    if (previous == null) {  //delete first entry node.
+                        table[hash] = table[hash].next;
+                        return true;
+                    } else {
+                        previous.next = current.next;
+                        return true;
+                    }
+                }
+                previous = current;
+                current = current.next;
+            }
+            return false;
+        }
+
+    }
 
 
+   public V get(K key) {
+        int hash = hash(key) % capacity;
+        if (table[hash] == null) {
+            return null;
+        } else {
+            HashMap<K, V> temp = table[hash];
+            while (temp != null) {
+                if (temp.key.equals(key))
+                    return temp.value;
+                temp = temp.next; //return value corresponding to key.
+            }
+            return null;   //returns null if key is not found.
+        }
+    }
+    public int size() {
+        return capacity;
+    }
 
-
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < table.length; i++) {
+            if (table[i] != null) {
+                HashMap<K, V> entry = table[i];
+                while (entry != null) {
+                    sb.append(entry.key + "=" + entry.value + " ");
+                    entry = entry.next;
+                }
+            }
+        }
+        return sb.toString();
+    }
 }
-
